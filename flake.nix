142f4ac795a11585642e59b5d129d64d5c1e33f1
Spyridon Passas <spyridonpassas0325@gmail.com>
2026-09-09 10:32:12 +0200

    setup: added flake.nix file

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Angular + .Net dev enviroment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs
+          dotnetCorePackages.sdk_9_0
+        ];
+        shellHook = ''
+          export DOTNET_ROOT="${pkgs.dotnetCorePackages.sdk_9_0}"
+          export PATH="$DOTNET_ROOT:$PATH"
+        '';
+      };
+    };
+}
